c280498101f757335d62761e85350e24bc7c9667
Aaron <trapp.root@gmail.com>
2024-10-22 21:53:38 -0700

    Slight refactor

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ import { nextMoves, printPath } from './helpers.js';
 const knightMoves = (start, end) => {
 	const [endX, endY] = end;
 	const paths = [[start]];
+	
 	while (true) {
 		const currentPath = paths.shift();
 		const lastPosition = currentPath[currentPath.length - 1];
 		const nextPosition = nextMoves(lastPosition);
+		
 		for (const next of nextPosition) {
 			const [nextX, nextY] = next;
 			const newPath = currentPath.slice();
 			
-			if (endX === nextX && endY === nextY) return printPath(newPath.concat([[nextX, nextY]]));
-			
+			if (endX === nextX && endY === nextY) {
+				newPath.push(next);
+				return printPath(newPath);
+			}
+
 			const newPathSet = new Set(...newPath);
 			if (newPathSet.has(next) === false) {
 				newPathSet.add(next);
